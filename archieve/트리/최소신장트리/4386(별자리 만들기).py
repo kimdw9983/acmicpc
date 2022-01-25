4be2ea8b21589708acadebe4d941f9b0e9397079
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2022-01-25 06:11:16 +0000

    팰린드롬¿

---
+++ archieve/트리/최소신장트리/4386(별자리 만들기).py
@@ -0,0 +1,38 @@
+import sys, heapq, math
+input = sys.stdin.readline
+V = int(input().rstrip()) 
+
+space = []
+for _ in range(V) :
+	space.append(tuple(map(float, input().split())))
+
+heap = []
+def add_edge(u) :
+	global heap
+	x1, y1 = space[u]
+	for i, coord in enumerate(space) :
+		if i == u : continue
+		x2, y2 = space[i]
+		d = math.hypot(x2-x1, y2-y1) #유클리드계에서 두점사이의 거리
+		heapq.heappush(heap, (d, i))
+
+check = [False] * V
+def prim() :
+	weight = 0
+	heapq.heappush(heap, (0.0, 0))
+	for i in range(V) :
+		while heap :
+			top = heap[0]
+			v = top[1]
+
+			if not check[v] :
+				w = top[0]
+				break
+			heapq.heappop(heap)
+
+		weight += w
+		check[v] = True
+		add_edge(v)
+	return weight
+
+print(prim())
