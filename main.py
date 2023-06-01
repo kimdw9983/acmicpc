1143122afbd9d1d09af87fa24554b740aa2d0eaa
kimdw9983 <kimdw9983@gmail.com>
2023-06-01 14:05:33 +0900

    0601

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ except :
 for fname in (
               "test", 
               # "test2", 
-              # "test3", 
+              "test3", 
               # "test4",
               # "test5",
               # "test6",
