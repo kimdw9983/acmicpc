35f0475a1379cd43077bf6d439eac004a7bf1f9e
kimdw9983 <kimdw9983@gmail.com>
2023-06-22 10:10:57 +0900

    0622

--- lib/nojam.py
+++ lib/nojam.py
@@ -99,6 +99,32 @@ def timeout(seconds=1, error_message=os.strerror(errno.ETIME)):
   return decorator
 
 __builtins__['timeout'] = timeout
+
+def get_size(obj, seen=None):
+  size = sys.getsizeof(obj)
+  if seen is None:
+    seen = set()
+  obj_id = id(obj)
+  if obj_id in seen:
+    return 0
+  seen.add(obj_id)
+  if isinstance(obj, dict):
+    size += sum([get_size(v, seen) for v in obj.values()])
+    size += sum([get_size(k, seen) for k in obj.keys()])
+  elif hasattr(obj, '__dict__'):
+    size += get_size(obj.__dict__, seen)
+  elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+    size += sum([get_size(i, seen) for i in obj])
+  return size
+
+def sizeof_fmt(num, suffix="B"):
+  for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+    if abs(num) < 1024.0:
+      return f"{num:3.1f}{unit}{suffix}"
+    num /= 1024.0
+  return f"{num:.1f}Yi{suffix}"
+
+__builtins__['size'] = lambda x: print(sizeof_fmt(get_size(x)))
 ##################################################
 """
 import resource
