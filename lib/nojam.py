f38b8bc19c23d31b628e9d508d15d3e283f06c59
kimdw9983 <kimdw9983@gmail.com>
2023-07-03 18:09:58 +0900

    코드 정리

--- lib/nojam.py
+++ lib/nojam.py
@@ -5,6 +5,54 @@ yellow = "\x1b[33;20m"
 red = "\x1b[31;20m"
 reset = "\x1b[0m"
 #####################################################
+import importlib, sys, time
+from types import ModuleType
+def get_module(name, package=None): #module객체를 실행시키지 않고 가지고 옴
+  #https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+  absolute_name = importlib.util.resolve_name(name, package)
+  module = sys.modules.get(absolute_name)
+  if module: return module
+
+  for finder in sys.meta_path:
+    spec = finder.find_spec(absolute_name, None)
+    if spec is not None:
+      break
+  if not spec : return
+  module = importlib.util.module_from_spec(spec)
+  sys.modules[absolute_name] = module
+
+  return module
+
+def invoke(module: ModuleType, fname: str):
+  global fp
+  tnum = 1
+  
+  while True:
+    try:
+      first_fp = fp
+      input() #f가 비었으면 예외가 발생함.
+    except StopIteration:
+      return
+
+    if first_fp :
+      tnum += 1
+    seek(first_fp)
+
+    prev_fp = first_fp
+    elapsed = time.time()
+    try :
+      judge_status[fname] = "AC"
+      importlib.reload(module)
+      status = judge_status[fname]
+      is_ac = status != "WA"
+      _print(f"{green if is_ac else red}[{status: ^4}] {blue}{module.__name__}.py{reset}\t{yellow}CASE {str(tnum)}{reset} elapsed time: {yellow}{time.time() - elapsed}{reset}")
+    except (NameError, SyntaxError, StopIteration) as e:
+      _print(f"{red}[FAIL] {blue}{module.__name__}.py{reset}\t{yellow}CASE {str(tnum)}{reset} elapsed time: {yellow}{time.time() - elapsed}\t{magenta}{type(e).__name__}{reset}")
+      # seek()
+    init_nprint()
+    if fp == prev_fp : #모듈을 실행했는데 파일포인터가 움직이지 않은 경우 -> 종료조건
+      return
+#####################################################
 import sys, datetime, os, io
 f = open("input.acmicpc", 'r+', encoding="utf-8", errors="ignore") #파일의 iterator를 공유한다.
 
